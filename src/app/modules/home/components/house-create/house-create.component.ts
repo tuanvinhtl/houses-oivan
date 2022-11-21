1f4f8b827a0f40c5ee3bffc87b7046e7e6a91ec9
vinh.nguyentuan <tuanvinhtl@gmail.com>
2022-11-21 13:35:38 +0700

    commit submit form

--- src/app/modules/home/components/house-create/house-create.component.ts
+++ src/app/modules/home/components/house-create/house-create.component.ts
@@ -1,9 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-house-create",
   templateUrl: "./house-create.component.html",
   styleUrls: ["./house-create.component.scss"],
 })
-export class HouseCreateComponent{
+export class HouseCreateComponent {
+  @Input() houseModels = [];
+  @Input() houseStatus = [];
+  @Input() houseTypes = [];
+
+  formGroup: FormGroup = new FormGroup({
+    houseNumber: new FormControl(),
+    blockNumber: new FormControl(),
+    landNumber: new FormControl(),
+    houseType: new FormControl(),
+    houseModel: new FormControl(),
+    housePrice: new FormControl(),
+    status: new FormControl(),
+  });
+
+  submit() {
+    if(this.formGroup.invalid){
+      return
+    }
+    
+    console.log(this.formGroup.value);
+  }
 }
--- src/app/modules/home/pages/house-management/house-management.component.ts
+++ src/app/modules/home/pages/house-management/house-management.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
 import { ApiService } from "src/app/core/services/api.service";
+import { Filter } from "src/app/shared/models/filter";
 import { HouseResponse } from "src/app/shared/models/house";
+import { HouseModelsCombiner } from "src/app/shared/models/house-models";
+import { HouseListingStateService } from "../../services/house-listing-state.service";
 
 @Component({
   selector: "app-house-management",
@@ -8,6 +12,9 @@ import { HouseResponse } from "src/app/shared/models/house";
   styleUrls: ["./house-management.component.scss"],
 })
 export class HouseManagementComponent implements OnInit {
-  constructor(private apiService: ApiService<{}, Array<HouseResponse>>) {}
+  houseListing$: Observable<HouseModelsCombiner[]> = this.houseListingStateService.houseListing$;
+  filterSource$: Observable<any> = this.houseListingStateService.filterSource$;
+  constructor(private houseListingStateService: HouseListingStateService) {
+  }
   ngOnInit(): void {}
 }
